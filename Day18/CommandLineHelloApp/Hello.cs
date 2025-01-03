090d5b1610b5f8a1fe0a2c0eea034a426e687b2e
Minu <pminu456@hotmail.com>
2025-01-03 20:48:02 +0530

    Day18 commit

---
+++ Day18/CommandLineHelloApp/Hello.cs
@@ -0,0 +1,22 @@
+﻿using System;
+class Hello
+{
+
+
+    static void Main(string[] userNames)
+    {
+
+        if (userNames.Length == 0)
+        {
+            Console.WriteLine("No arguments passed,pls pass argments");
+            return;
+        }
+        foreach (string userName in userNames)
+        {
+            Console.WriteLine("Hello, " + userName + "! changed");
+        }
+
+
+
+    }
+}
