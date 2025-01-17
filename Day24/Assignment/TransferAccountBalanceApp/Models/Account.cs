33ef105c528e9c558aaca35ea805a7f8a9834ef8
Minu <pminu456@hotmail.com>
2025-01-17 09:23:33 +0530

    Day 24 commit

---
+++ Day24/Assignment/TransferAccountBalanceApp/Models/Account.cs
@@ -0,0 +1,52 @@
+﻿
+namespace TransferAccountBalanceApp.Models
+{
+    class Account
+    {
+        private readonly int _accountNumber;
+        private readonly string _name;
+        private double _balance;
+        
+        public Account(int accountNumber, string name, double balance)
+        {
+            _accountNumber = accountNumber;
+            _name = name;
+            _balance = balance;
+        }
+
+        public double Balance
+        {
+            get { return _balance; }
+        }
+
+        public string Name
+        {
+            get { return _name; }
+        }
+
+        public int AccountNumber
+        {
+            get { return _accountNumber; }
+        }
+
+        public bool Deposit(double amount)
+        {
+            if (amount > 0)
+            {
+                _balance += amount;
+                return true;
+            }
+            return false;
+        }
+
+        public bool Withdraw(double amount)
+        {
+            if (amount > 0 && amount <= _balance)
+            {
+                _balance -= amount;
+                return true;
+            }
+            return false;
+        }
+    }
+}
---
+++ Day24/Assignment/TransferAccountBalanceApp/Program.cs
@@ -0,0 +1,37 @@
+﻿using System.Security.Principal;
+using TransferAccountBalanceApp.Models;
+
+namespace TransferAccountBalanceApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Account minuAccount = new Account(1, "Minu", 2000);
+            Account sachinAccount = new Account(2, "Sachin", 3000);
+
+            // Perform deposit and withdraw operations
+            bool depositResult = minuAccount.Deposit(500);
+            DisplayTransactionResult(minuAccount, "deposit", 500, depositResult);
+
+            bool withdrawResult = sachinAccount.Withdraw(1000);
+            DisplayTransactionResult(sachinAccount, "withdraw", 1000, withdrawResult);
+        }
+
+        static void DisplayTransactionResult(Account account, string transactionType, double amount, bool success)
+        {
+            if (!success)
+            {
+                Console.WriteLine($"Failed to {transactionType} {amount} to/from {account.Name}'s account.");
+                return;
+            }
+
+            Console.WriteLine($"{transactionType}ed {amount} to/from {account.Name}'s account. New balance: {account.Balance}");
+        }
+    }
+    }
+
+
+
+
+
