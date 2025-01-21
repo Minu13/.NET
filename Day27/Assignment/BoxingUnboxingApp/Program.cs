f39ee56b180742a37cbe0c69db42c1870ba38fa4
Minu <pminu456@hotmail.com>
2025-01-21 22:03:36 +0530

    Day27 Commit

---
+++ Day27/Assignment/BoxingUnboxingApp/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace BoxingUnboxingApp
+{
+    class Program
+    {
+        static void Main()
+        {
+            object[] objectArray = new object[] { 10, 20, 30, 40, 50 };
+
+            int sum = 0;
+            int max = int.MinValue;
+            int min = int.MaxValue;
+
+
+            for (int i = 0; i < objectArray.Length; i++)
+            {
+                int num = (int)objectArray[i]; // Unboxing
+                sum += num;
+
+                if (num > max)
+                {
+                    max = num;
+                }
+
+                if (num < min)
+                {
+                    min = num;
+                }
+            }
+
+            Console.WriteLine("Sum: " + sum);
+            Console.WriteLine("Max: " + max);
+            Console.WriteLine("Min: " + min);
+        }
+    }
+}
