9cec9c58e62b46ebd848e0eb7e0c3d0ed003209d
Minu <pminu456@hotmail.com>
2025-01-21 09:05:15 +0530

    Day26 commit

---
+++ Day26/Assignment/StaticEmployeeIDApp/Models/Employee.cs
@@ -0,0 +1,31 @@
+﻿
+namespace StaticEmployeeIDApp.Models
+{
+    class Employee
+    {
+        private static int lastID = 0;
+        private int _employeeID;
+        private string _name;
+
+        public Employee(string name)
+        {
+            _employeeID = ++lastID;
+            _name = name;
+        }
+
+        public int EmployeeID
+        {
+            get
+            {
+                return _employeeID;
+            }
+        }
+        public string Name
+        {
+            get
+            {
+                return _name;
+            }
+        }
+    }
+}
---
+++ Day26/Assignment/StaticEmployeeIDApp/Program.cs
@@ -0,0 +1,18 @@
+﻿using StaticEmployeeIDApp.Models;
+
+namespace StaticEmployeeIDApp
+{
+     class Program
+    {
+        static void Main(string[] args)
+        {
+            var e1 = new Employee("Deepa");
+            var e2 = new Employee("Minu");
+            var e3 = new Employee("Markonda");
+
+            Console.WriteLine("Employee1 ID:" + e1.EmployeeID);
+            Console.WriteLine("Employee2 ID:" + e2.EmployeeID);
+            Console.WriteLine("Employee3 ID:" + e3.EmployeeID);
+        }
+    }
+}
