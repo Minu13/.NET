c1abc9b8fd0196701e6f0684b1bb319b77b400d0
Minu <pminu456@hotmail.com>
2025-01-23 09:07:06 +0530

    Day 28 Commit

---
+++ Day28/Assignment/DesignFunctionApp/Models/Account.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignFunctionApp.Models
+{
+    class Account
+    {
+        //fields
+        private int _id;
+        private string _name;
+        private double _amount;
+
+        //Constructors
+        public Account() { }
+        public Account(int id, string name, double amount)
+        {
+            _id = id;
+            _name = name;
+            _amount = amount;
+        }
+
+        //Properties
+       public int Id
+        {
+            get
+            { 
+            return _id;
+            }
+        }
+
+        public string Name
+        {
+            get
+            {
+                return _name;
+            }
+        }
+        public double Amount
+        {
+            get
+            {
+                return _amount;
+            }
+        }
+
+        //Methods
+        public void Deposit(double amount) 
+        {
+        }
+        public void Withdraw(double amount)
+        { 
+        }
+
+        public int GetMethodCount()
+        {
+            return 2;
+        }  
+        public int GetConstructorCount()
+        {
+            return 2;
+        } 
+        public int GetPropertyCount()
+        {
+            return 3;
+        } 
+    }
+}
---
+++ Day28/Assignment/DesignFunctionApp/Program.cs
@@ -0,0 +1,17 @@
+﻿using DesignFunctionApp.Models;
+
+namespace DesignFunctionApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Account account = new Account();
+
+            Console.WriteLine("Type: Account");
+            Console.WriteLine($"Number of Methods:{account.GetMethodCount()}");
+            Console.WriteLine($"Number of Properties: {account.GetPropertyCount()}");
+            Console.WriteLine($"Number of Constructors: {account.GetConstructorCount()}");
+        }
+    }
+}
