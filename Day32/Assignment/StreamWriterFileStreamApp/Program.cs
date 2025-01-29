833f89e73e946189fbc6768c9110caf42e270935
Minu <pminu456@hotmail.com>
2025-01-29 09:02:24 +0530

    Day 32 commit

---
+++ Day32/Assignment/StreamWriterFileStreamApp/Program.cs
@@ -0,0 +1,26 @@
+﻿using StreamWriterFileStreamApp.Models;
+
+namespace StreamWriterFileStreamApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter your username: ");
+            string username = Console.ReadLine();
+
+            Console.WriteLine("Enter your address:");
+            string address = Console.ReadLine();
+
+            string filePath = @"C:\minu-html-app\StreamWriter\foo.txt";
+
+            FileWriter filewriter = new FileWriter(filePath);
+
+            filewriter.WriteToFile(username, address);
+
+            filewriter.Close();
+
+            Console.WriteLine("Your data has been saved to C:\\minu-html-app\\StreamWriter\\foo.txt"); 
+        }
+    }
+}
---
+++ Day32/Coding Excercise/ReverseArrayApp/Program.cs
@@ -0,0 +1,45 @@
+﻿namespace ReverseArrayApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            {
+                string[] array = { "mmm", "rrr", "sss", "ttt", "ddd" };
+
+                Console.WriteLine("Original array:");
+                PrintArray(array);
+
+                ReverseArray(array);
+
+                Console.WriteLine("Reversed array:");
+                PrintArray(array);
+            }
+
+            static void ReverseArray(string[] array)
+            {
+                int left = 0;
+                int right = array.Length - 1;
+
+                while (left < right)
+                {
+                    string temp = array[left];
+                    array[left] = array[right];
+                    array[right] = temp;
+
+                    left++;
+                    right--;
+                }
+            }
+
+            static void PrintArray(string[] array)
+            {
+                foreach (string element in array)
+                {
+                    Console.Write(element + " ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
